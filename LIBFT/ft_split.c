cdca9ec2edb1d41e2f6bfd373d677ac81d3d879e
edogarci <131349717+edogarci@users.noreply.github.com>
2023-04-20 11:42:34 +0200

    split terminado

--- LIBFT/ft_split.c
+++ LIBFT/ft_split.c
@@ -1,83 +1,121 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: edogarci <edogarci@student.42malaga.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/19 16:30:51 by edogarci          #+#    #+#             */
-/*   Updated: 2023/04/19 16:30:51 by edogarci         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <stddef.h>
-#include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+static int get_num_of_substr(char const *s, char c, int pos)
+{
+    int cont;
+
+    cont = 0;
+    if (s[pos] == '\0')
+    {
+        cont = 1;
+        return (cont);
+    }
+    else
+    {
+        while (pos >= 0)
+        {
+            if (s[pos] == c || s[pos] == '\0')
+                cont++;
+            pos--;
+        }
+        cont++;
+        return (cont);
+    }
+}
+
+static void assign_substr_to_ptr(char const *s, char *ptr, int start, int end)
+{
+    int cont;
+
+    cont = 0;
+    while (start < end)
+    {
+        ptr[cont] = s[start];
+        start++;
+        cont++;
+    }
+    ptr[cont] = '\0';
+//    printf("Substr: %s\n", ptr);
+}
+
+static void split_substrs(char const *s, char del, char **ptr, int len)
+{
+    int start;
+    int end;
+    int cont;
+    int pos;
+    int size;
+
+/*    ptr[0] = "split 1";
+    ptr[1] = "split 2";
+    ptr[2] = "split 3";
+    printf("%s\n", ptr[0]);
+    printf("%s\n", ptr[1]);
+    printf("%s\n", ptr[2]);*/
+
+    pos = 0;
+    cont = 0;
+    start = 0;
+    end = 0;
+    while (pos <= len)
+    {
+        if (s[pos] == del || s[pos] == '\0')
+        {
+            size = end - start;
+            ptr[cont] = malloc((size + 1) * sizeof(char));
+            assign_substr_to_ptr(s, ptr[cont], start, end);
+            cont++;
+            start = end + 1;
+        }
+        end++;
+        pos++;
+    }
+    ptr[cont] = NULL;
+/*    printf("%s\n", ptr[0]);
+    printf("%s\n", ptr[1]);
+    printf("%s\n", ptr[2]);*/
+}
 
 char **ft_split(char const *s, char c)
 {
-	int len_s;
-    int cont;
-    int del_cont;
-	int start;
-	int end;
-	int len;
-	char *ptr;
-	int cont2;
-    char **ptr_array;
-    int conter;
+    int num_sub_strs;
+    int s_len;
+    char **ptr_arrays;
 
-	//longitud de s
-	len_s = 0;
-	while (s[len_s] != '\0')
-		len_s++;
+    //obtenemos longitud del string
+    //SUSTITUIR con ft_strlen
+    s_len = 0;
+    while (s[s_len] != '\0')
+        s_len++;
 
-    //creamos el array de punteros
-    del_cont = 1; //al menos 1 para el puntero a NULL
-    cont = 0;
-    while (cont <= len_s)
-    {
-        if (s[cont] == c)
-            del_cont++;
-        cont++;
-    }
-    del_cont++;
-    ptr_array = malloc(del_cont * (sizeof (char *)));
+    //obtener numero de substrings para reservar
+    //malloc del puntero principal
+    //+1 para un puntero NULL
+    num_sub_strs = get_num_of_substr(s, c, s_len);
+    ptr_arrays = malloc(num_sub_strs * sizeof(char *));
 
-	cont = 0;
-	start = cont;
-	end = start;
-    conter = 0;
-	while (cont <= len_s) {
-        //delimitador encontrado
-        if (s[cont] == c) {
-            //creamos el array
-            len = end - start;
-            ptr = malloc((len + 1) * sizeof(char));
-            cont2 = 0;
-            //asignamos el substring al array creado
-            while (start <= end) {
-                if (start == end)
-                {
-                    ptr[cont2] = '\0';
-                    ptr_array[conter] = ptr;
-                    conter++;
-                }
-                else
-                    ptr[cont2] = s[start];
-                cont2++;
-                start++;
-            }
-        } else {
-            end++;
-        }
-        cont++;
-    }
+    //recorremos el string, separando substrings
+    //y asignadolo en los distintos espacios del
+    //ptr de arrays
+    split_substrs(s, c, ptr_arrays, s_len);
+/*    ptr_arrays[0] = "test1";
+    ptr_arrays[1] = "edorta";
+    ptr_arrays[2] = "paco";
+    printf("%s\n", ptr_arrays[0]);
+    printf("%s\n", ptr_arrays[1]);
+    printf("%s\n", ptr_arrays[2]);
+*/
+    //retornamos el puntero de arrays
+    return (ptr_arrays);
 }
 
 int main(void)
 {
-    ft_split("esto,es,una,cadena", ',');
+    char c;
+    char **ptr;
+
+    c = ',';
+    ptr = ft_split("f1,f2,f3", c);
     return (0);
 }
